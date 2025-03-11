606ab92ce260603c271964e808687657b8d4abc1
Notlim11 <miltonjaydalumpines@gmail.com>
2025-03-11 17:44:34 +0800

    hehe

---
+++ html/index.js
@@ -0,0 +1,20 @@
+let display = document.getElementByld("display");
+
+// Function to append values to the display
+function appendToDisplay(value) {
+display.value += value;
+}
+
+// Function to clear the display
+function clearDisplay() {
+display.value = **;
+}
+
+// Function to evaluate the expression
+function calculate() {
+try {
+ display.value = eval(display.value);
+ } catch (error) {
+ alert("invalid expressionl");
+ }
+}
